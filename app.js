6fdef78f02101972ab617a77e2eac224b26dff40
Aakash-Naykude <aakashnaykude123@gmail.com>
2021-11-28 17:42:57 +0530

    .

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const user = require("./MOCK_DATA.json")
 const app = express()
 app.use(express.json())
 const connect = ()=>{
@@ -66,68 +65,86 @@ app.delete("/user/:id", async (req, res)=>{
 })
 //------------------- CRUD for user end ---------------------------------
 
-//-----------------------Schema for section ---------------------------------
-const sectionSchema = new mongoose.Schema({
-    typeOfBook : {type:String , required:true},
-    nameofbook:{type:String, required:true},
-    status:{type:String, required:false},
-    book_id:{
+
+
+//------------------- Schema for author ---------------------------------
+const authorSchema = new mongoose.Schema({
+    book_name:{type:String, required:true},
+    author_name:{type:String, required:true},
+    user_id:{
         type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "books"
+        required:true,
+        ref:"user"
     },
-    author_name:{
-        type: String,
-        required:true
+},
+    {
+        versionKey:false,
+        timestamps:true
     }
-},{
-    versionKey:false,
-    timestamps:true
-})
-const Section = mongoose.model("section", sectionSchema)
-//-------------------- CRUD FOR section ---------------------------
-app.post("/section", async (req, res)=>{
+)
+const Author = mongoose.model("author", authorSchema)
+//-----------------------CRUM author-------------------
+app.post("/author", async (req, res)=>{
     try{
-        const user = await Section.create(req.body)
+        const user = await Author.create(req.body)
         return res.status(201).send(user)
     } catch(e){
         return res.status(500).json({message: e.message, status:"Failed"})
     }
 })
-app.get("/section", async (req, res)=>{
+app.get("/author", async (req, res)=>{
     try{
-        const user = await Section.find().lean().exec()
+        const user = await Author.find().lean().exec()
         return res.status(201).send(user)
     } catch(e){
         return res.status(500).json({message: e.message, status:"Failed"})
     }
 })
-app.get("/section/:par", async (req, res)=>{
+app.get("/author/:par", async (req, res)=>{
     try{
-        console.log(req.params.par)
-        const user = await Section.find({$or: [{"status":{$eq:req.params.par}}, {"author_name":{$eq:req.params.par}}]}).lean().exec()
+        //console.log(req.params.par)
+        const user = await Author.find({"author_name":{$eq:req.params.par}}).lean().exec()
+        return res.status(201).send({user})
+    } catch(e){
+        return res.status(500).json({message: e.message, status:"Failed"})
+    }
+})
+app.get("/author/:id/author_name", async (req, res)=>{
+    try{
+        const user = await Author.findById(req.params.id).lean().exec()
+        const user2 = await Author.find(user_ids = user.user_id).lean().exec()
+        //use populate(user_id)
+        return res.status(201).send({user, user2})
+    } catch(e){
+        return res.status(500).json({message: e.message, status:"Failed"})
+    }
+})
+app.get("/author/:id", async (req, res)=>{
+    try{
+        const user = await Author.findById(req.params.id).lean().exec()
         return res.status(201).send(user)
     } catch(e){
         return res.status(500).json({message: e.message, status:"Failed"})
     }
 })
-app.patch("/section/:id", async (req, res)=>{
+app.patch("/author/:id", async (req, res)=>{
     try{
-        const user = await Section.findByIdAndUpdate(req.params.id, req.body, {new:true}).lean().exec()
+        const user = await Author.findByIdAndUpdate(req.params.id, req.body, {new:true}).lean().exec()
         return res.status(201).send(user)
     } catch(e){
         return res.status(500).json({message: e.message, status:"Failed"})
     }
 })
-app.delete("/section/:id", async (req, res)=>{
+app.delete("/author/:id", async (req, res)=>{
     try{
-        const user = await Section.findByIdAndDelete(req.params.id).lean().exec()
+        const user = await Author.findByIdAndDelete(req.params.id).lean().exec()
         return res.status(201).send(user)
     } catch(e){
         return res.status(500).json({message: e.message, status:"Failed"})
     }
 })
-//--------------------- CRUD For section end --------------------------
+//--------------------------------CRUD for author end --------------------------------
+
 
 
 //--------------------- Schema For book start --------------------------
@@ -200,89 +217,69 @@ app.delete("/books/:id", async (req, res)=>{
 
 
 
-//------------------- Schema for author ---------------------------------
-const authorSchema = new mongoose.Schema({
-    book_name:{type:String, required:true},
-    author_name:{type:String, required:true},
-    user_id:{
-        type: mongoose.Schema.Types.ObjectId,
-        required:true,
-        ref:"user"
-    },
-    section_id:{
-        type: mongoose.Schema.Types.ObjectId,
-        required:true,
-        ref: "section"
-    }
-    },
-    {
-        versionKey:false,
-        timestamps:true
-    }
-)
-const Author = mongoose.model("author", authorSchema)
-//-----------------------CRUM author-------------------
-app.post("/author", async (req, res)=>{
-    try{
-        const user = await Author.create(req.body)
-        return res.status(201).send(user)
-    } catch(e){
-        return res.status(500).json({message: e.message, status:"Failed"})
-    }
-})
-app.get("/author", async (req, res)=>{
-    try{
-        const user = await Author.find().lean().exec()
-        return res.status(201).send(user)
-    } catch(e){
-        return res.status(500).json({message: e.message, status:"Failed"})
-    }
-})
-app.get("/author/:par", async (req, res)=>{
-    try{
-        //console.log(req.params.par)
-        const user = await Author.find({"author_name":{$eq:req.params.par}}).lean().exec()
-        return res.status(201).send({user})
-    } catch(e){
-        return res.status(500).json({message: e.message, status:"Failed"})
-    }
-})
-app.get("/author/:id/author_name", async (req, res)=>{
-    try{
-        const user = await Author.findById(req.params.id).lean().exec()
-        const user2 = await Author.find(user_ids = user.user_id).lean().exec()
-        //use populate(user_id)
-        return res.status(201).send({user, user2})
-    } catch(e){
-        return res.status(500).json({message: e.message, status:"Failed"})
-    }
-})
-app.get("/author/:id", async (req, res)=>{
-    try{
-        const user = await Author.findById(req.params.id).lean().exec()
-        return res.status(201).send(user)
-    } catch(e){
-        return res.status(500).json({message: e.message, status:"Failed"})
-    }
-})
-app.patch("/author/:id", async (req, res)=>{
-    try{
-        const user = await Author.findByIdAndUpdate(req.params.id, req.body, {new:true}).lean().exec()
-        return res.status(201).send(user)
-    } catch(e){
-        return res.status(500).json({message: e.message, status:"Failed"})
-    }
-})
-app.delete("/author/:id", async (req, res)=>{
-    try{
-        const user = await Author.findByIdAndDelete(req.params.id).lean().exec()
-        return res.status(201).send(user)
-    } catch(e){
-        return res.status(500).json({message: e.message, status:"Failed"})
-    }
-})
-//--------------------------------CRUD for author end --------------------------------
 
+//-----------------------Schema for section ---------------------------------
+const sectionSchema = new mongoose.Schema({
+    typeOfBook : {type:String , required:true},
+    nameofbook:{type:String, required:true},
+    status:{type:String, required:false},
+    book_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "books"
+    },
+    author_name:{
+        type: String,
+        required:true
+    }
+},{
+    versionKey:false,
+    timestamps:true
+})
+const Section = mongoose.model("section", sectionSchema)
+//-------------------- CRUD FOR section ---------------------------
+app.post("/section", async (req, res)=>{
+    try{
+        const user = await Section.create(req.body)
+        return res.status(201).send(user)
+    } catch(e){
+        return res.status(500).json({message: e.message, status:"Failed"})
+    }
+})
+app.get("/section", async (req, res)=>{
+    try{
+        const user = await Section.find().populate("book_id").lean().exec()
+        return res.status(201).send(user)
+    } catch(e){
+        return res.status(500).json({message: e.message, status:"Failed"})
+    }
+})
+app.get("/section/:par", async (req, res)=>{
+    try{
+        console.log(req.params.par)
+        const user = await Section.find({$or: [{"status":{$eq:req.params.par}}, {"author_name":{$eq:req.params.par}}]}).lean().exec()
+        return res.status(201).send(user)
+    } catch(e){
+        return res.status(500).json({message: e.message, status:"Failed"})
+    }
+})
+app.patch("/section/:id", async (req, res)=>{
+    try{
+        const user = await Section.findByIdAndUpdate(req.params.id, req.body, {new:true}).lean().exec()
+        return res.status(201).send(user)
+    } catch(e){
+        return res.status(500).json({message: e.message, status:"Failed"})
+    }
+})
+app.delete("/section/:id", async (req, res)=>{
+    try{
+        const user = await Section.findByIdAndDelete(req.params.id).lean().exec()
+        return res.status(201).send(user)
+    } catch(e){
+        return res.status(500).json({message: e.message, status:"Failed"})
+    }
+})
+//--------------------- CRUD For section end --------------------------
 
 
 
